f02fe3ae98a2b95c50f71c7f703a6076d91e71b9
JuHyun Seong <ernestogevar@naver.com>
2015-04-19 00:12:04 +0900

    Git test

---
+++ src/de/vogella/git/first/GitTest.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package de.vogella.git.first;
+
+/**
+ * @author seongjuhyeon
+ *
+ */
+public class GitTest {
+	public static void main(String[] args) {
+		System.out.println("Git is fun");
+	}
+}
